460c39eebeb2418179da4cbf8578ad9f0eac42f5
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-28 18:12:25 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,7 +8,7 @@ class EmailAddressParser
   attr_accessor :email_addresses
 
   def initialize(emails)
-    @email_addresses = emails.split(', ').split(' ')
+    @email_addresses = emails.split(', ')
   end
 
   def parse
